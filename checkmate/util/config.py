38ef9cf34b118337de3d9730022680c33524b0e5
Austin Mordahl <austin.mordahl@utdallas.edu>
2021-12-29 13:28:23 -0600

    Update config.py

--- checkmate/util/config.py
+++ checkmate/util/config.py
@@ -11,15 +11,15 @@ def __get_configuration():
                 if k != "variables":
                     for k1, v1 in raw_content['variables'].items():
                         raw_content[k] = raw_content[k].replace(k1, raw_content[v1])
-
-        for _, v in raw_content.items():
-            if not os.path.exists(v):
-                raise FileNotFoundError(f'Could not find file {v} from config.json')
-            
-        return raw_content
     except FileNotFoundError as fe:
         logging.critical("Config file not found. Looking in "
                          "./resources/config.json. Try running again from "
                          "the checkmate root directory?")
+    for _, v in raw_content.items():
+        if not os.path.exists(v):
+            raise FileNotFoundError(f'Could not find file {v} from config.json')
+
+    return raw_content
+
 
 configuration = __get_configuration()
