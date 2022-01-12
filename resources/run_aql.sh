b0bf046f91f07e03f95b74011677099b256e4d7c
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-01-12 16:48:48 -0600

    Update run_aql.sh

--- resources/run_aql.sh
+++ resources/run_aql.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 set -x
-java -jar -Xms3g -Xmx6g AQL-System-1.1.0-SNAPSHOT.jar -t 2h -reset -c ${1} -q "Flows IN App('${2}') ?" -o ${3}
+java -jar -Xms3g -Xmx6g AQL-System-1.1.0-SNAPSHOT.jar -t 1m -reset -c ${1} -q "Flows IN App('${2}') ?" -o ${3}
