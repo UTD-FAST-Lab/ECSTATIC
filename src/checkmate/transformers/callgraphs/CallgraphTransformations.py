1c146562d6b974ab89fd2b6989007da2b8e96ca1
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-02 17:53:21 -0500

    Fixed violation computation

--- src/checkmate/transformers/callgraphs/CallgraphTransformations.py
+++ src/checkmate/transformers/callgraphs/CallgraphTransformations.py
@@ -1,3 +1,5 @@
+from typing import Set, Dict, List
+
 from networkx import DiGraph
 
 from src.checkmate.readers.callgraph.CGNode import CGNode
@@ -12,3 +14,13 @@ def to_context_insensitive(graph: DiGraph) -> DiGraph:
         v_prime = CGNode(v.content, None)
         new_graph.add_edge(u_prime, v_prime)
     return new_graph
+
+def call_site_to_targets(graph: DiGraph) -> Dict[str, Set[str]]:
+    result: Dict[str, Set[str]] = dict()
+    for caller, targets in graph.adjacency():
+        caller: CGNode
+        targets: List[CGNode]
+        if caller.site not in result:
+            result[caller.site] = set()
+        result[caller.site].update([k.content for k in targets])
+    return result
--- src/checkmate/violation_checkers/CallgraphViolationChecker.py
+++ src/checkmate/violation_checkers/CallgraphViolationChecker.py
@@ -1,8 +1,10 @@
+import dataclasses
 from typing import Any
 
 from networkx import DiGraph
 
 from src.checkmate.readers.callgraph.CallGraphReader import CallGraphReader
+from src.checkmate.transformers.callgraphs import CallgraphTransformations
 from src.checkmate.util.UtilClasses import Violation, FinishedFuzzingJob
 from src.checkmate.violation_checkers.AbstractViolationChecker import AbstractViolationChecker
 
@@ -11,8 +13,8 @@ class CallgraphViolationChecker(AbstractViolationChecker):
     def is_more_precise(self, job1: FinishedFuzzingJob, job2: FinishedFuzzingJob) -> Violation:
         result1 = self.read_from_input(job1.results_location)
         result2 = self.read_from_input(job2.results_location)
-        adj1 = {k: v for k, v in result1.adjacency()}
-        adj2 = {k: v for k, v in result2.adjacency()}
+        adj1 = CallgraphTransformations.call_site_to_targets(result1)
+        adj2 = CallgraphTransformations.call_site_to_targets(result2)
         all_differences = list()
         for k, v in adj1.items():
             if k not in adj2:
@@ -21,15 +23,14 @@ class CallgraphViolationChecker(AbstractViolationChecker):
                 differences = set(adj2[k]) - set(v)
                 if len(differences) > 0:
                     all_differences.extend([f'{k} -> {v1.content}' for v1 in differences])
-        return Violation(len(all_differences) > 0, "precision", job1, job2, all_differences)
-
+        return Violation(len(all_differences) > 0, "precision", dataclasses.asdict(job1), dataclasses.asdict(job2),
+                         all_differences)
 
     def is_more_sound(self, job1: FinishedFuzzingJob, job2: FinishedFuzzingJob) -> Violation:
         result1 = self.read_from_input(job1.results_location)
         result2 = self.read_from_input(job2.results_location)
-
-        adj1 = {k: v for k, v in result1.adjacency()}
-        adj2 = {k: v for k, v in result2.adjacency()}
+        adj1 = CallgraphTransformations.call_site_to_targets(result1)
+        adj2 = CallgraphTransformations.call_site_to_targets(result2)
         all_differences = list()
         for k, v in adj1.items():
             if k not in adj2:
@@ -38,7 +39,8 @@ class CallgraphViolationChecker(AbstractViolationChecker):
                 differences = set(v) - set(adj2[k])
                 if len(differences) > 0:
                     all_differences.extend([f'{k} -> {v1.content}' for v1 in differences])
-        return Violation(len(all_differences) > 0, "soundness", job1, job2, all_differences)
+        return Violation(len(all_differences) > 0, "soundness", dataclasses.asdict(job1), dataclasses.asdict(job2),
+                         all_differences)
 
     def read_from_input(self, file: str) -> DiGraph:
         return CallGraphReader().import_graph(file)
