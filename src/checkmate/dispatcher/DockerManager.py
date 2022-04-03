fc99a535eebf43abe791bb36767d46287c0e188c
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-03 08:58:25 -0500

    Update DockerManager.py

--- src/checkmate/dispatcher/DockerManager.py
+++ src/checkmate/dispatcher/DockerManager.py
@@ -34,6 +34,7 @@ def start_runner(tool: str, benchmarks: List[str], tasks: List[str]):
     # run build benchmark script
     command = f'tester {tool} {" ".join(benchmarks)} -t {" ".join(tasks)}'
     cntr = client.create_container(image=get_image_name(tool), command=command)
+    logging.info(f"Cntr is {cntr}")
     strm, stat = cntr.get_archive(cntr, "/results")
     with open(os.path.join(importlib.resources.path("results", ""),
                            f"{tool}_{'-'.join(benchmarks)}_{'-'.join(tasks)}_{time.time()}.tar")) as f:
