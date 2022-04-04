8b88ee24b11e2110813272e78e6f2921e0268b73
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-04 13:46:38 -0500

    Fix violation detection

--- src/checkmate/violation_checkers/CallgraphViolationChecker.py
+++ src/checkmate/violation_checkers/CallgraphViolationChecker.py
@@ -15,12 +15,17 @@ class CallgraphViolationChecker(AbstractViolationChecker):
         adj2 = CallgraphTransformations.call_site_to_targets(result2)
         all_differences = list()
         for k, v in adj1.items():
-            if k not in adj2:
-                all_differences.extend([f'{k} -> {v1}' for v1 in v])
-            else:
-                differences = adj2[k] - v
-                if len(differences) > 0:
-                    all_differences.extend([f'{k} -> {v1}' for v1 in differences])
+            """
+            For each call site, if job1 is more precise than job2, we expect job1's
+            targets to be a subset of job2's. So violations are anything in
+            job1 - job2
+            
+            If we have a callsite in adj1 that is not in adj2, then all of its
+            sites are violations.
+            """
+            if k in adj2:
+                all_differences.extend([f'{k} -> {v1}' for v1 in v.difference(
+                    (adj2[k] if k in adj2 else set()))])
         return Violation(len(all_differences) > 0, "precision", job1, job2, all_differences)
 
     def is_more_sound(self, job1: FinishedFuzzingJob, job2: FinishedFuzzingJob) -> Violation:
@@ -29,13 +34,18 @@ class CallgraphViolationChecker(AbstractViolationChecker):
         adj1 = CallgraphTransformations.call_site_to_targets(result1)
         adj2 = CallgraphTransformations.call_site_to_targets(result2)
         all_differences = list()
-        for k, v in adj1.items():
-            if k not in adj2:
-                all_differences.extend([f'{k} -> {v1}' for v1 in v])
-            else:
-                differences = v - adj2[k]
-                if len(differences) > 0:
-                    all_differences.extend([f'{k} -> {v1}' for v1 in differences])
+        for k, v in adj2.items():
+            """
+            For each call site, if job1 is more sound than job2, then we expect
+            job2's targets to be a subset of job1's. Thus, a violation is anything in
+            job2 - job1.
+            
+            If there is a callsite in adj2 that is not in adj1, then all of the out-edges
+            are a violation.
+            """
+            if k in adj1:
+                all_differences.extend([f'{k} -> {v1}' for v1 in v.difference(
+                    (adj1[k] if k in adj1 else set()))])
         return Violation(len(all_differences) > 0, "soundness", job1, job2, all_differences)
 
     def read_from_input(self, file: str) -> DiGraph:
