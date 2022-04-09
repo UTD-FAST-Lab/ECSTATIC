6b6ce1d593d96a556c6b9c585226567331c79c84
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-09 12:35:34 -0500

    Added srcZip option to WALA runner

--- src/checkmate/runners/WALARunner.py
+++ src/checkmate/runners/WALARunner.py
@@ -17,4 +17,4 @@ class WALARunner(CommandLineToolRunner):
             raise NotImplementedError(f'WALA does not support task {task}.')
 
     def get_base_command(self) -> List[str]:
-        return "java -jar /WALAInterface/target/WALAInterface-1.0-jar-with-dependencies.jar".split(" ")
+        return "java -jar /WALAInterface/target/WALAInterface-1.0-jar-with-dependencies.jar --srcZips /usr/lib/jvm/openjdk-11/lib/src.zip".split(" ")
