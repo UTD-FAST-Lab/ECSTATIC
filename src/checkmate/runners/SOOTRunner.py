900d701a787d7e9262a4144cb59ad369aa8cec3e
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-15 17:12:05 -0500

    Update SOOTRunner.py

--- src/checkmate/runners/SOOTRunner.py
+++ src/checkmate/runners/SOOTRunner.py
@@ -41,4 +41,4 @@ class SOOTRunner(CommandLineToolRunner):
             return rest_of_config
 
     def get_base_command(self) -> List[str]:
-        return "java -jar /SootInterface/target/SootInterface-1.0-SNAPSHOT-jar-with-dependencies.jar --keep-line-number -pp -w --src-lines -p cg.spark enabled:true".split(" ")
+        return "java -jar /SootInterface/target/SootInterface-1.0-SNAPSHOT-jar-with-dependencies.jar --keep-line-number -pp -w -p cg.spark enabled:true".split(" ")
