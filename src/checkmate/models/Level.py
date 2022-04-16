3affc726706124f65b38d602866381cc202c7876
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-16 16:13:25 -0500

    Update Level.py

--- src/checkmate/models/Level.py
+++ src/checkmate/models/Level.py
@@ -5,5 +5,7 @@ class Level:
     option_name: str
     level_name: str
 
+    def __hash__(self):
+        return hash((self.option_name, self.level_name))
     def __str__(self):
         return f"{self.option_name}.{self.level_name}"
