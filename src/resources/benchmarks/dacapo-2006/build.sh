6355aa854e9693993d30dfca4866321cac481def
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-16 14:42:53 -0500

    Update build.sh

--- src/resources/benchmarks/dacapo-2006/build.sh
+++ src/resources/benchmarks/dacapo-2006/build.sh
@@ -8,5 +8,5 @@ git clone https://bitbucket.org/yanniss/doop-benchmarks.git --depth 1
 cd /doop-benchmarks/dacapo-2006
 mkdir /benchmarks/dacapo-2006
 find . -type f -name '*.jar' | parallel cp -t /benchmarks/dacapo-2006
-cp src/resources/benchmarks/dacapo-2006/index.json /benchmarks/dacapo-2006
+cp /checkmate/src/resources/benchmarks/dacapo-2006/index.json /benchmarks/dacapo-2006
 cd $CUR
