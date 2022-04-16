2dd797b0a8fe8453ba45620837c17cd6af88a1be
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-16 14:42:27 -0500

    Update build.sh

--- src/resources/benchmarks/dacapo-2006/build.sh
+++ src/resources/benchmarks/dacapo-2006/build.sh
@@ -9,3 +9,4 @@ cd /doop-benchmarks/dacapo-2006
 mkdir /benchmarks/dacapo-2006
 find . -type f -name '*.jar' | parallel cp -t /benchmarks/dacapo-2006
 cp src/resources/benchmarks/dacapo-2006/index.json /benchmarks/dacapo-2006
+cd $CUR
