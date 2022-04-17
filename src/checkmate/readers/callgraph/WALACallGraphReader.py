14ca1581ae1eedd098ea17a5745d2255c12363e2
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-17 15:18:52 -0500

    Update WALACallGraphReader.py

--- src/checkmate/readers/callgraph/WALACallGraphReader.py
+++ src/checkmate/readers/callgraph/WALACallGraphReader.py
@@ -1,3 +1,4 @@
+import re
 from typing import Tuple
 
 from src.checkmate.readers.callgraph.AbstractCallGraphReader import AbstractCallGraphReader
@@ -10,12 +11,30 @@ class WALACallGraphReader(AbstractCallGraphReader):
         """
         A WALA line looks like:
 
-        < Primordial, Lsun/reflect/generics/reflectiveObjects/TypeVariableImpl, hashCode()I >	invokevirtual < Primordial, Ljava/lang/Object, hashCode()I >@4	Everywhere	synthetic < Primordial, Ljava/lang/Object, hashCode()I >	Everywhere
-        @param line:
+        < Primordial, Ljava/lang/Class, toString()Ljava/lang/String; >	java/lang/Class:156	Everywhere	java.lang.Class.getName()Ljava/lang/String;	Everywhere
+        < Primordial, Ljava/lang/Class, toString()Ljava/lang/String; >	java/lang/Class:156	Everywhere	java.lang.StringBuilder.append(Ljava/lang/String;)Ljava/lang/StringBuilder;	Everywhere        @param line:
         @return:
         """
+        tokens = line.split('\t')
+        callsite = tokens[0].split(',')[1]
+        callsite = CGCallSite(callsite.split(':')[0], callsite.split(':')[1], tokens[2])
+        java_signature_regex = re.compile(r"(.*?).([A-z_\-]*?)\((.*?)\)(.*)")
+        matches = java_signature_regex.match(tokens[3])
+        target = CGTarget(matches.group(1), matches.group(2),
+                          self.parse_type(matches.group(4)),
+                          [self.parse_type(t) for t in matches.group(3).split(';')],
+                          tokens[4])
+        return callsite, target
 
-        pass
+    def parse_type(self, type: str) -> str:
+        postfix = ""
+        for charix in range(len(type)):
+            if type[charix] == '[':
+                postfix = postfix + "[]"
+            elif type[charix] == "L":
+                return type[charix + 1:].strip(";") + postfix
+            else:
+                return self.map_character_to_type(type[charix])
 
     def map_character_to_type(self, character: str) -> str:
         types = {
