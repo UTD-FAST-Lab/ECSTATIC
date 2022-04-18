83c69a1527d9a2c2431eb9901fc662d122f873dc
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-17 19:17:37 -0500

    Fix source download

--- src/resources/benchmarks/dacapo-2006/build.sh
+++ src/resources/benchmarks/dacapo-2006/build.sh
@@ -13,11 +13,14 @@ cp /checkmate/src/resources/benchmarks/dacapo-2006/index.json /benchmarks/dacapo
 # Clone sources
 cd /
 wget https://sourceforge.net/projects/dacapobench/files/archive/2006-10-MR2/dacapo-2006-10-MR2-src.zip
-unzip dacapo-2006-10-MR2-src.zip
-cd benchmarks
+mkdir dacapo-2006-10-MR2-src
+unzip -d dacapo-2006-10-MR2-src dacapo-2006-10-MR2-src.zip
+cd dacapo-2006-10-MR2-src/benchmarks
 cp /checkmate/src/resources/benchmarks/dacapo-2006/build.xml .
 ant sources
 
 find . -type f -name "*.tar.gz" | parallel cp -t /benchmarks/dacapo-2006
 find . -type f -name "*.zip" | parallel cp -t /benchmarks/dacapo-2006
+find . -type f -name '*.jar' | parallel cp -t /benchmarks/dacapo-2006
+find . -type f -name '*.tgz' | parallel cp -t /benchmarks/dacapo-2006
 cd $CUR
