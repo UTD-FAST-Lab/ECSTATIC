5c1de3702719690b5c25c3f235e999ae1d44c031
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-20 18:36:50 -0500

    Update WALA Call graph reader

--- src/checkmate/readers/callgraph/WALACallGraphReader.py
+++ src/checkmate/readers/callgraph/WALACallGraphReader.py
@@ -16,35 +16,6 @@ class WALACallGraphReader(AbstractCallGraphReader):
         @return:
         """
         tokens = line.split('\t')
-        callsite = tokens[0].split(',')[1]
-        callsite = CGCallSite(callsite.split(':')[0], callsite.split(':')[1], tokens[2])
-        java_signature_regex = re.compile(r"(.*?).([A-z_\-]*?)\((.*?)\)(.*)")
-        matches = java_signature_regex.match(tokens[3])
-        target = CGTarget(matches.group(1), matches.group(2),
-                          self.parse_type(matches.group(4)),
-                          [self.parse_type(t) for t in matches.group(3).split(';')],
-                          tokens[4])
+        callsite = CGCallSite(tokens[0], tokens[1], tokens[2])
+        target = CGCallSite(tokens[3], tokens[4])
         return callsite, target
-
-    def parse_type(self, type: str) -> str:
-        postfix = ""
-        for charix in range(len(type)):
-            if type[charix] == '[':
-                postfix = postfix + "[]"
-            elif type[charix] == "L":
-                return type[charix + 1:].strip(";") + postfix
-            else:
-                return self.map_character_to_type(type[charix])
-
-    def map_character_to_type(self, character: str) -> str:
-        types = {
-            'B': 'byte',
-            'C': 'char',
-            'D': 'double',
-            'F': 'float',
-            'I': 'int',
-            'J': 'long',
-            'S': 'short',
-            'Z': 'boolean'
-        }
-        return types[character]
