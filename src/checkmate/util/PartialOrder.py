b777f46e6303c654910807be14556476f02e5540
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-22 15:36:50 -0500

    Remove circular import

--- src/checkmate/util/PartialOrder.py
+++ src/checkmate/util/PartialOrder.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import Enum
 
 from src.checkmate.models.Level import Level
-from src.checkmate.models.Option import Option
 
 
 class PartialOrderType(Enum):
