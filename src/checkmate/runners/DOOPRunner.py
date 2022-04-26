3cd6741f3d384e6e30e10602fd7c142ef6c28768
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-26 10:38:51 -0500

    Add DOOP timeout

--- src/checkmate/runners/DOOPRunner.py
+++ src/checkmate/runners/DOOPRunner.py
@@ -25,7 +25,7 @@ class DOOPRunner(CommandLineToolRunner):
             raise NotImplementedError(f'DOOP does not support task {task}.')
 
     def get_base_command(self) -> List[str]:
-        return ["doop"]
+        return ["doop", "-t", "120"]
 
     def run_from_cmd(self, cmd: List[str], job: FuzzingJob, output_file: str):
         cmd.extend(self.get_input_option(job.target))
