63b532c425ebe7a9cde2d990fa7839534ad52431
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-27 18:03:07 -0500

    Fix Flowdroid script

--- src/resources/tools/flowdroid/flowdroid.sh
+++ src/resources/tools/flowdroid/flowdroid.sh
@@ -1,2 +1,2 @@
-#!/bin/zsh
+#!/bin/bash
 java -Xmx${1}g -jar %FLOWDROID_HOME%/soot-infoflow-cmd/target/soot-infoflow-cmd-jar-with-dependencies.jar -a ${2} -p ${3} -s %SOURCE_SINK_LOCATION% %CONFIG% > ${4} 2>&1
