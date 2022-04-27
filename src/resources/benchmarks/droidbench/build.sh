a4155d7d47cca3689cbefdd1a419cb5a085848ff
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-04-26 22:58:09 -0500

    Add FlowDroid support

--- src/resources/benchmarks/droidbench/build.sh
+++ src/resources/benchmarks/droidbench/build.sh
@@ -3,7 +3,9 @@
 apt install -y parallel
 CURDIR=$(pwd)
 cd /
-git clone https://github.com/secure-software-engineering/DroidBench/tree/develop
+git clone https://github.com/secure-software-engineering/DroidBench.git
+cd DroidBench
+git checkout develop
 mkdir -p /benchmarks/droidbench
-find ./DroidBench/apk -maxdepth 1 -type d | grep -v InterAppCommunication | parallel mv -t ./benchmarks/droidbench
+find .apk -maxdepth 1 -type d | grep -v InterAppCommunication | parallel mv -t ./benchmarks/droidbench
 cd $CURDIR
