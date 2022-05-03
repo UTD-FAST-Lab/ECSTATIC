074b765d65b8cf2abcec9f4c16b415a53b6ba829
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-05-03 12:56:56 -0500

    Fix test benchmark

--- src/resources/benchmarks/test/build.sh
+++ src/resources/benchmarks/test/build.sh
@@ -5,7 +5,7 @@ CUR=$(pwd)
 mkdir -p /benchmarks/test
 cd /benchmarks/test
 git clone https://github.com/amordahl/CATS-Microbenchmark.git
-cd ./CATS-Microbenchmark/benchmarks/Reflection/TrivialReflection/TR1
+cd ./CATS-Microbenchmark/benchmarks/Unsafe/Unsafe1
 
 # Build repo
 mvn clean compile package
