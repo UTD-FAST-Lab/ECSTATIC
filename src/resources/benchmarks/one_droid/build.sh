192ea25ef4ac574a0d758053a4fe67025215c434
Pancax <dakotasoles@gmail.com>
2022-06-02 13:08:13 -0500

    fix one_droid

--- src/resources/benchmarks/one_droid/build.sh
+++ src/resources/benchmarks/one_droid/build.sh
@@ -5,7 +5,9 @@ cd /
 mkdir -p benchmarks
 cd /benchmarks
 git clone https://github.com/Pancax/droidbench_android_projects.git
-cd droidbench_android_projects
-cd ActivityEventSequence2
-gradlew assembleDebug
+mkdir one_droid
+mv droidbench_android_projects/ActivityEventSequence2 one_droid
+mv droidbench_android_projects/build-all.sh one_droid
+cd one_droid
+./build-all.sh
 cd $CURDIR
