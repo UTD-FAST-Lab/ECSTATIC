a72fb9b2fe452e80d6aa5ef798a9bad90e97f2a5
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-07-23 13:29:09 -0500

    Try Pathos for parallelism with dill

--- src/ecstatic/violation_checkers/AbstractViolationChecker.py
+++ src/ecstatic/violation_checkers/AbstractViolationChecker.py
@@ -15,22 +15,6 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#  CheckMate: A Configuration Tester for Static Analysis
-#
-#
-#  This program is free software: you can redistribute it and/or modify
-#      it under the terms of the GNU General Public License as published by
-#      the Free Software Foundation, either version 3 of the License, or
-#      (at your option) any later version.
-#
-#      This program is distributed in the hope that it will be useful
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#      GNU General Public License for more details.
-#
-#      You should have received a copy of the GNU General Public License
-#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import json
 import logging
 import os.path
@@ -38,7 +22,7 @@ import pathlib
 import pickle
 import time
 from abc import ABC, abstractmethod
-from multiprocessing.dummy import Pool
+from pathos.multiprocessing import Pool
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import List, Tuple, Set, Iterable, TypeVar, Optional
