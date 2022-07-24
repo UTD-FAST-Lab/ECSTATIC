0199bc0ed3525be2696eac17e912cd91009e9bc5
egsch <egschaumann@gmail.com>
2022-07-23 20:48:50 -0500

    try saving delta debugging filename

--- src/ecstatic/debugging/JSBenchmarkDeltaDebugger.py
+++ src/ecstatic/debugging/JSBenchmarkDeltaDebugger.py
@@ -42,9 +42,9 @@ class JSBenchmarkDeltaDebugger(JavaBenchmarkDeltaDebugger, ABC):
 
         with tempfile.NamedTemporaryFile(mode='w', dir=directory, delete=False) as f:
             f.write("#!/bin/bash\n")
+            f.write("echo $1 > /home/delta_filename.txt\n")
             cmd = f"deltadebugger {job_tmp.name}"
             f.write(cmd + "\n")
-            f.write("echo $1")
             f.write("exit (echo !$?)\n")
             result = f.name
             logger.info(f"Wrote cmd {cmd} to delta debugging script.")
