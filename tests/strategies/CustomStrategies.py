e51bc1d7eaa96df67ee1914a4f0e45701c7cdd40
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-07-24 14:15:23 -0500

    Lift partial order strategy

--- tests/strategies/CustomStrategies.py
+++ tests/strategies/CustomStrategies.py
@@ -30,27 +30,29 @@ def categorical_option(draw,
                        min_levels=0,
                        max_levels=100,
                        min_level_settings=0,
-                       max_level_settings=100):
+                       max_level_settings=100,
+                       partial_order_strategy=strategies.one_of(strategies.just(None),
+                                       strategies.just(PartialOrderType.MORE_SOUND_THAN),
+                                       strategies.just(PartialOrderType.MORE_PRECISE_THAN))):
     levels = draw(strategies.lists(
         strategies.lists(strategies.text(), min_size=min_level_settings, max_size=max_level_settings),
         min_size=min_levels,
         max_size=max_levels))
     option = Option(name)
     for level_list in levels:
-        state = draw(strategies.one_of(strategies.just(None),
-                                       strategies.just(PartialOrderType.MORE_SOUND_THAN),
-                                       strategies.just(PartialOrderType.MORE_PRECISE_THAN)))
+        state = draw(partial_order_strategy)
         for level in level_list:
             option.add_level(level)
         for ix in range(len(level_list) - 1):
             match state:
                 case PartialOrderType.MORE_SOUND_THAN:
-                    option.set_more_sound_than(level_list[ix], level_list[ix+1])
+                    option.set_more_sound_than(level_list[ix], level_list[ix + 1])
                 case PartialOrderType.MORE_PRECISE_THAN:
-                    option.set_more_precise_than(level_list[ix], level_list[ix+1])
+                    option.set_more_precise_than(level_list[ix], level_list[ix + 1])
                 case None:
                     pass
     return option
+
 
 @strategies.composite
 def option(draw):
@@ -58,6 +60,7 @@ def option(draw):
     Add more potential draws here if I implement other strategies.
     """
     return draw(categorical_option)
+
 
 @strategies.composite
 def tool(draw, name=strategies.text(),
