f545561dba72378a730b87cac485c04c6f1f92e8
egsch <egschaumann@gmail.com>
2022-07-24 09:04:11 -0500

    echo target locations to file

--- src/ecstatic/debugging/JSBenchmarkDeltaDebugger.py
+++ src/ecstatic/debugging/JSBenchmarkDeltaDebugger.py
@@ -41,7 +41,7 @@ class JSBenchmarkDeltaDebugger(JavaBenchmarkDeltaDebugger, ABC):
 
         with tempfile.NamedTemporaryFile(mode='w', dir=directory, delete=False) as f:
             f.write("#!/bin/bash\n")
-            f.write(f"echo $1 \n {job.potential_violation.job1.job.target} \n {job.potential_violation.job2.job.target}> /home/delta_filename.txt\n")
+            f.write(f"echo -e \"$1 {job.potential_violation.job1.job.target} {job.potential_violation.job2.job.target}> /home/delta_filename.txt\n\"")
             cmd = f"deltadebugger {job_tmp.name}"
             f.write(cmd + "\n")
             f.write("exit (echo !$?)\n")
