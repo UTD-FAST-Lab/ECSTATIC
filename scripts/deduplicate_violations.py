dac358fdcb90b47a1704cee680eff716e079789a
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-08-25 19:25:28 -0500

    Add script to detect duplicates

---
+++ scripts/deduplicate_violations.py
@@ -0,0 +1,61 @@
+#  ECSTATIC: Extensible, Customizable STatic Analysis Tester Informed by Configuration
+#
+#  Copyright (c) 2022.
+#
+#  This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import argparse
+import logging
+import os
+from pathlib import Path
+from typing import Iterable, Tuple, Set
+
+from src.ecstatic.util.PartialOrder import PartialOrder, PartialOrderType
+
+p = argparse.ArgumentParser()
+p.add_argument("directory", help="The violations directory")
+args = p.parse_args()
+
+
+def main():
+    def get_json_files(directory: Path) -> Iterable[Path]:
+        for root, dirs, files in os.walk(directory):
+            for f in files:
+                if f.endswith(".json"):
+                    yield Path(root) / Path(f)
+
+    def parse_file_name(file: Path) -> Tuple[str, frozenset[str], str]:
+        tokens = file.absolute().name.split('/')
+        benchmark = tokens[-1]
+        second_po = '/'.join(tokens[-4:-1])
+        first_po = '/'.join(tokens[-7:-4])
+        if first_po[1] not in ['MST', 'MPT']:
+            logging.info(f"{file} only has one partial order.")
+        else:
+            option_name = tokens[-8]
+            return option_name, frozenset({first_po, second_po}), benchmark
+
+    seen = {}
+    to_delete = []
+    for f in sorted(get_json_files(args.directory)):
+        fingerprint_tuple = parse_file_name(f)
+        if fingerprint_tuple not in seen:
+            seen[fingerprint_tuple] = f.absolute().name
+        else:
+            logging.critical(f"{f} is a duplicate of {seen[fingerprint_tuple]}")
+            to_delete.append(f)
+
+
+if __name__ == '__main__':
+    main()
