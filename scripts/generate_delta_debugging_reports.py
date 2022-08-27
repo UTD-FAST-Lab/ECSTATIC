fd8ea4b683e73101792c7c5f17dfd41b9b1bb3b5
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-08-27 15:57:45 -0500

    fixed script

--- scripts/generate_delta_debugging_reports.py
+++ scripts/generate_delta_debugging_reports.py
@@ -14,10 +14,13 @@ def generate_record(line: str) -> str:
                 return f"{type},{get_filename_attributes(rest[1:])}"
             case ["DIRECT", *rest]:
                 return f"{'/'.join(rest[2:-3])},{rest[-3]}"
+            case [head, *rest]:
+                return get_filename_attributes(rest)
 
     line = Path(line).absolute()
-    return get_filename_attributes(line.split('/'))
+    return get_filename_attributes(str(line).split('/'))
 
 
 if __name__ == "__main__":
-    map(print, map(generate_record, sys.stdin))
+    for line in sys.stdin:
+        print(generate_record(line))
