9fbf80c1860be4ce0805b2b1fb60070cc60a4726
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-08-27 22:00:18 -0500

    Fix fuzzer state output

--- scripts/generate_csvs.py
+++ scripts/generate_csvs.py
@@ -47,7 +47,7 @@ def main(file: str):
 
         return reduce1(str(file).split('/'))
 
-    return generate_comma_separated_record(Path(file))
+    return generate_comma_separated_record(Path(file).absolute())
 
 if __name__ == "__main__":
     for file in sys.stdin:
