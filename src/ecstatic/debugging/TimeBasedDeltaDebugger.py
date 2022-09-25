f6622abf2201a1b077e35c508170c42da5bee520
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-09-25 13:56:23 -0500

    Reduce delta debugging time

--- src/ecstatic/debugging/TimeBasedDeltaDebugger.py
+++ src/ecstatic/debugging/TimeBasedDeltaDebugger.py
@@ -123,7 +123,7 @@ class TimeBasedDeltaDebugger():
 
         # Then, create the script.
         def predicate(f: FinishedFuzzingJob) -> bool:
-            return f.execution_time_in_seconds >= self.maximum_time_in_seconds * 1000
+            return f.execution_time_in_seconds >= self.maximum_time_in_seconds
 
         job = DeltaDebuggingJob(predicate=predicate,
                                 runner=self.runner,
