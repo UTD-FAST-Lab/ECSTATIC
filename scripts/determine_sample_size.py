6d1f2170c8dd1e232d75288255be6d749b3a6ede
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-11-18 21:51:39 -0600

    Script

--- scripts/determine_sample_size.py
+++ scripts/determine_sample_size.py
@@ -9,6 +9,8 @@ args = p.parse_args()
 def main():
     benchmark_sample = set()
     partial_order_sample = set()
+    total_benchmarks = 0
+    total_partial_orders = 0
     for fi in args.files:
         with open(fi) as f:
             record = json.load(f)
@@ -16,8 +18,8 @@ def main():
             benchmark_sample.update(bs)
         for po in record['partial_order_sample']:
             partial_order_sample.update(po)
-        total_benchmarks = len(record['benchmarks'])
-        total_partial_orders = len(record['partial_orders'].keys())
+        total_benchmarks = max(total_benchmarks, len(record['benchmarks']))
+        total_partial_orders = max(total_partial_orders, len(record['partial_orders'].keys()))
 
     print(f"Number of benchmarks: {len(benchmark_sample)}/{total_benchmarks}")
     print(f"Number of partial orders: {len(partial_order_sample)}/{total_partial_orders}")
