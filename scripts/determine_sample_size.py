f643b91bc14b64f930fe1a12948fbf68cd715f69
Austin Mordahl <austin.mordahl@utdallas.edu>
2022-11-18 21:47:33 -0600

    Fix output

---
+++ scripts/determine_sample_size.py
@@ -0,0 +1,26 @@
+import argparse
+import json
+
+p = argparse.ArgumentParser()
+p.add_argument("files", nargs="+")
+args = p.parse_args()
+
+
+def main():
+    benchmark_sample = set()
+    partial_order_sample = set()
+    for fi in args.files:
+        with open(fi) as f:
+            record = json.load(f)
+        for bs in record['benchmarks_sample']:
+            benchmark_sample.update(bs)
+        for po in record['partial_order_sample']:
+            partial_order_sample.update(po)
+        total_benchmarks = len(record['benchmarks'])
+        total_partial_orders = len(record['partial_orders'].keys())
+
+    print(f"Number of benchmarks: {len(benchmark_sample)}/{total_benchmarks}")
+    print(f"Number of partial orders: {len(partial_order_sample)}/{total_partial_orders}")
+
+if __name__ == '__main__':
+    main()
